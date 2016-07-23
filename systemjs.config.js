1642e1eb77b2f5abdf9d64f7d0695c5566d51b83
Irina Krehovetska <ira.krehovetska@gmail.com>
2016-07-23 15:46:59 +0300

    Fixed libraries

--- systemjs.config.js
+++ systemjs.config.js
@@ -8,13 +8,15 @@
         'app': 'app/dist/js', // 'dist',
         '@angular': 'node_modules/@angular',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs': 'node_modules/rxjs'
+        'rxjs': 'node_modules/rxjs',
+        'angular2-jwt': 'node_modules/angular2-jwt/angular2-jwt.js'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app': { main: 'main.js', defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
+        "angular2-jwt": { defaultExtension: "js" }
     };
     var ngPackageNames = [
         'common',
